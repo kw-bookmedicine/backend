b77d054d52314c623413b9b283092382e8922fac
simjaeyoun <spqjfgogo@gmail.com>
2024-01-02 02:32:17 +0900

    init : '초기 프로젝트 생성'

---
+++ BookPharmacy/src/test/java/kr/KWGraduate/BookPharmacy/BookPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.KWGraduate.BookPharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
