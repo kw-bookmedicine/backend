8d4a9a00cb994e8233a3f2997ae8f5d86c1f6f88
simjaeyoun <spqjfgogo@gmail.com>
2024-01-02 19:25:58 +0900

    feat : 'example controller생성'

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/ExampleController.java
@@ -0,0 +1,12 @@
+package kr.KWGraduate.BookPharmacy;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExampleController {
+    @RequestMapping()
+    public String hello(){
+        return "hello";
+    }
+}
