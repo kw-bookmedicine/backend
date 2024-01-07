704798c0c029ad8c7bf8c250ec7f08059846f648
simjaeyoun <spqjfgogo@gmail.com>
2024-01-07 21:31:17 +0900

    test : 'spring jpa로 변경으로 인한 테스트 코드 변경'

--- src/test/java/kr/KWGraduate/BookPharmacy/repository/ClientRepositoryTest.java
+++ src/test/java/kr/KWGraduate/BookPharmacy/repository/ClientRepositoryTest.java
@@ -5,42 +5,40 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @SpringBootTest
 @Transactional
 class ClientRepositoryTest {
+
     @Autowired
     ClientRepository clientRepository;
 
     @Test
-    public void testSaveClient(){
-        Client cli1 = new Client("123","4321","sim","bob","spqjf", Client.Gender.F, Client.Occupation.UNEMPLOYED);
-
-        String saveId = clientRepository.save(cli1);
-        Assertions.assertThat(cli1.getId()).isEqualTo(saveId);
-    }
-    @Test
     public void integratedTest(){
         Client cli1 = new Client("123","4321","ha","bob","spqjf", Client.Gender.F, Client.Occupation.UNEMPLOYED);
-        String saveId1 = clientRepository.save(cli1);
+        Client saveClient1 = clientRepository.save(cli1);
 
         Client cli2 = new Client("124","4321","sim","bob","spqjf", Client.Gender.F, Client.Occupation.UNEMPLOYED);
-        String saveId2 = clientRepository.save(cli2);
+        Client saveClient2 = clientRepository.save(cli2);
 
         Assertions.assertThat(clientRepository.count()).isEqualTo(2);
 
         List<Client> allClients = clientRepository.findAll();
-        Assertions.assertThat(allClients).containsExactly(cli1,cli2);
 
-        String findCli1Name = clientRepository.findById(saveId1).getName();
-        Assertions.assertThat(findCli1Name).isEqualTo(cli1.name);
+
+        Assertions.assertThat(allClients).containsExactly(saveClient1,saveClient2);
+
+        String savedId = saveClient1.getId();
+
+        String findCli1Name = clientRepository.findById(savedId).get().getName();
+        Assertions.assertThat(findCli1Name).isEqualTo(cli1.getName());
+
+        org.junit.jupiter.api.Assertions.assertThrows(NoSuchElementException.class,() -> clientRepository.findById("sdkfjhsdfkghjfd").get().getName());
 
 
         clientRepository.delete(cli1);
@@ -48,6 +46,8 @@ class ClientRepositoryTest {
         Assertions.assertThat(clientRepository.count()).isEqualTo(1);
 
     }
+
+
     @Test
     public void findByEmailAndNickname(){
         Client cli1 = new Client("123","4321","ha","bob","spqjf", Client.Gender.F, Client.Occupation.UNEMPLOYED);
@@ -60,11 +60,12 @@ class ClientRepositoryTest {
         Assertions.assertThat(clientRepository.count()).isEqualTo(2);
 
 
-        List<Client> nickClients = clientRepository.findByNickname(cli1.getNickname());
-        Assertions.assertThat(nickClients).containsExactly(cli1);
+        Optional<Client> nickClient = clientRepository.findByNickname(cli1.getNickname());
 
-        List<Client> emailClients = clientRepository.findByEmail(cli2.getEmail());
-        Assertions.assertThat(emailClients).containsExactly(cli2);
+        Assertions.assertThat(nickClient.get().getId()).isEqualTo(cli1.getId());
+
+        Optional<Client> emailClient = clientRepository.findByEmail("dfgdf");
+        Assertions.assertThat(emailClient.isEmpty()).isEqualTo(true);
 
     }
 
