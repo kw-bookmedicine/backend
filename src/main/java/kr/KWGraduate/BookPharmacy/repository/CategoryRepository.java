b06559352157b7e7d3653c68162f7ede7955cab9
seonghooni <lsh3717@naver.com>
2024-01-09 14:37:56 +0900

    feat: Category Repository, Service, Controller, Dto 계층 작성

--- src/main/java/kr/KWGraduate/BookPharmacy/repository/CategoryRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/repository/CategoryRepository.java
@@ -2,6 +2,22 @@ package kr.KWGraduate.BookPharmacy.repository;
 
 import kr.KWGraduate.BookPharmacy.entity.Categories;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<Categories, Long> {
+
+    /**
+     * 부모 카테고리 이름으로 자식 카테고리 조회하기
+     * */
+    @Query("select c from Categories c join fetch c.parentCategory pc where pc.name = :categoryName")
+    List<Categories> findChildrenByBigCategoryName(@Param("categoryName")String bigCategoryName);
+
+    /**
+     * 대분류(level=1)들을 조회하기
+     * */
+    @Query("select c from Categories c where c.level = 1")
+    List<Categories> findBigCategory();
 }
