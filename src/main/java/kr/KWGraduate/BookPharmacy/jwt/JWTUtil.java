e99cf87171ecbb1361601cf93cd862e29009e251
simjaeyoun <spqjfgogo@gmail.com>
2024-02-05 16:19:43 +0900

    refactor : 토킅 생성 시 시간 제한을 인자로 받는 것이 아닌 final변수로 선언

--- src/main/java/kr/KWGraduate/BookPharmacy/jwt/JWTUtil.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/jwt/JWTUtil.java
@@ -2,19 +2,19 @@ package kr.KWGraduate.BookPharmacy.jwt;
 
 import io.jsonwebtoken.Jwts;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
 import java.util.Date;
 
 @Component
 public class JWTUtil {
     private SecretKey secretKey;
+    private final Long ACESS_TOKEN_EXPIRATION_TIME_SECOND = 60L; // 5분
 
     public JWTUtil( @Value("${spring.jwt.secret}") String secret) {
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
@@ -31,12 +31,12 @@ public class JWTUtil {
         return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
     }
 
-    public String createJwt(String username, String role, Long expiredMs){
+    public String createJwt(String username, String role){
         return Jwts.builder()
                 .claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .expiration(Date.from(OffsetDateTime.now().plusSeconds(ACESS_TOKEN_EXPIRATION_TIME_SECOND).toInstant()))
                 .signWith(secretKey)
                 .compact();
     }
