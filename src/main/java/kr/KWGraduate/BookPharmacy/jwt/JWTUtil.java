5d65e9973da3f6f95cff651fe7f4c6e9733e5f81
simjaeyoun <spqjfgogo@gmail.com>
2024-02-19 20:41:17 +0900

    refactor : 토큰의 유효기간 값을 환경변수로 받아오는 것으로 변경

--- src/main/java/kr/KWGraduate/BookPharmacy/jwt/JWTUtil.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/jwt/JWTUtil.java
@@ -3,20 +3,12 @@ package kr.KWGraduate.BookPharmacy.jwt;
 import io.jsonwebtoken.*;
 
 import jakarta.servlet.http.HttpServletRequest;
-import kr.KWGraduate.BookPharmacy.dto.client.ClientDetails;
 import kr.KWGraduate.BookPharmacy.dto.token.TokenDto;
-import kr.KWGraduate.BookPharmacy.exception.status.AllException;
 import kr.KWGraduate.BookPharmacy.service.ClientDetailsService;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.header.Header;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -24,23 +16,26 @@ import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.SignatureException;
 import java.time.OffsetDateTime;
-import java.time.OffsetTime;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Component
 public class JWTUtil {
-    private SecretKey secretKey;
-    private final Long ACESS_TOKEN_EXPIRATION_TIME_SECOND = 60L; // 5분
-    private final Long REFRESH_TOKEN_EXPIRATION_TIME_SECOND = 300L; // 5분
+    private final SecretKey secretKey;
+    private final long ACCESS_TOKEN_EXPIRATION_TIME_SECOND;
+    private final long REFRESH_TOKEN_EXPIRATION_TIME_SECOND;
 
     private ClientDetailsService clientDetailsService;
 
-    public JWTUtil( @Value("${spring.jwt.secret}") String secret, ClientDetailsService clientDetailsService) {
+    public JWTUtil(
+            @Value("${spring.jwt.secret}") String secret,
+            @Value("${spring.jwt.access-token-expiration}") long accessTokenExpiration,
+            @Value("${spring.jwt.refresh-token-expiration}") long refreshTokenExpiration,
+            ClientDetailsService clientDetailsService) {
+
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
         //암호화 방식은 Jwts.SIG.HS256
+        ACCESS_TOKEN_EXPIRATION_TIME_SECOND = accessTokenExpiration;
+        REFRESH_TOKEN_EXPIRATION_TIME_SECOND = refreshTokenExpiration;
         this.clientDetailsService = clientDetailsService;
     }
 //    public String getUsername(String token){
@@ -65,7 +60,7 @@ public class JWTUtil {
                 //.claim("username", username)
                 .claim("role", role)
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(Date.from(OffsetDateTime.now().plusSeconds(ACESS_TOKEN_EXPIRATION_TIME_SECOND).toInstant()))
+                .expiration(Date.from(OffsetDateTime.now().plusSeconds(ACCESS_TOKEN_EXPIRATION_TIME_SECOND).toInstant()))
                 .signWith(secretKey)
                 .compact();
         String refreshToken = Jwts.builder()
