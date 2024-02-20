d92dd9c5287bccf9b58926e10be4683743d1fcef
simjaeyoun <spqjfgogo@gmail.com>
2024-02-20 15:59:51 +0900

    feat : redis에 저장될 entity설정

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/entity/redis/RefreshToken.java
@@ -0,0 +1,23 @@
+package kr.KWGraduate.BookPharmacy.entity.redis;
+
+import lombok.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
+
+@Getter
+@RedisHash(value = "refreshToken",timeToLive = 60)
+@NoArgsConstructor
+@EqualsAndHashCode
+@Builder
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    private String loginId;
+    private String refreshToken;
+
+    private String accessToken;
+
+}
