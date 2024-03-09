f2de067104d4122a149e15eebc55a9e1a2617e49
simjaeyoun <spqjfgogo@gmail.com>
2024-03-09 22:05:50 +0900

    refactor : 검증과정 삭제 및 로그인 로그아웃 fakecontroller 작성

--- src/main/java/kr/KWGraduate/BookPharmacy/controller/UserController.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/controller/UserController.java
@@ -1,6 +1,7 @@
 package kr.KWGraduate.BookPharmacy.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.KWGraduate.BookPharmacy.dto.client.ClientJoinDto;
 import kr.KWGraduate.BookPharmacy.dto.client.ClientLoginDto;
 import kr.KWGraduate.BookPharmacy.service.ClientService;
@@ -13,42 +14,25 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Client")
 public class UserController {
     private final ClientService clientService;
 
-    @Operation(summary = "로그인", description = "id와 password로 로그인 확인", tags = {"login"})
+    @Operation(summary = "로그인",description = "id와 password로 로그인")
     @PostMapping("/login")
-    public ResponseEntity<ClientLoginDto> login(@Validated @RequestBody ClientLoginDto request,
-                                BindingResult bindingResult){
-        if (bindingResult.hasErrors()) {
-            FieldError fieldError = bindingResult.getFieldError();
-            String errorMessage = new StringBuffer("login validation error")
-                    .append(" field: ").append(fieldError.getField())
-                    .append(" code ").append(fieldError.getCode())
-                    .append(" message: ").append(fieldError.getDefaultMessage())
-                    .toString();
-            System.out.println(errorMessage);
-
-            return ResponseEntity.badRequest().build();
-        }
-        //front에서 해결가능?
-        ClientLoginDto body = clientService.Login(request.getUsername(), request.getPassword());
-        return ResponseEntity.ok(body);
+    public void fakeLogin(@RequestBody ClientLoginDto request){
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
     }
-    @Operation(summary = "회원가입", description = "id, password, 이름, 닉네임, 이메일, 성별, 직업만 받도록 함", tags = {"signup"})
-    @PostMapping("/signup")
-    public ResponseEntity<String> signUp(@Validated @RequestBody ClientJoinDto clientJoinDto, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
-            FieldError fieldError = bindingResult.getFieldError();
-            String errorMessage = new StringBuffer("signUp validation error")
-                    .append(" field: ").append(fieldError.getField())
-                    .append(" code ").append(fieldError.getCode())
-                    .append(" message: ").append(fieldError.getDefaultMessage())
-                    .toString();
-            System.out.println(errorMessage);
 
-            return ResponseEntity.badRequest().build();
-        }
+    @Operation(summary = "로그아웃" , description = "쿠키 유효기간 만료시킴")
+    @GetMapping("/logout")
+    public void fakeLogout(){
+        throw new IllegalStateException("This method shouldn't be called. It's implemented by Spring Security filters.");
+    }
+
+    @Operation(summary = "회원가입" , description = "id, password, 이름, 닉네임, 이메일, 성별, 직업만 받도록 함")
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody ClientJoinDto clientJoinDto){
         clientService.signUp(clientJoinDto);
         return ResponseEntity.ok("success");
     }
