82e309da591ea93ac2985eee110b02d2665cdfeb
simjaeyoun <spqjfgogo@gmail.com>
2024-03-10 01:26:58 +0900

    refactor : 1. 회원정보를 검증하는 메서드 변경 2. 회원정보를 update하는 메서드 securityContext를 통해 변경

--- src/main/java/kr/KWGraduate/BookPharmacy/service/ClientService.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/service/ClientService.java
@@ -1,18 +1,25 @@
 package kr.KWGraduate.BookPharmacy.service;
 
 import kr.KWGraduate.BookPharmacy.dto.ClientDto;
+import kr.KWGraduate.BookPharmacy.dto.client.AuthenticationAdapter;
 import kr.KWGraduate.BookPharmacy.dto.client.ClientJoinDto;
 import kr.KWGraduate.BookPharmacy.dto.client.ClientLoginDto;
+import kr.KWGraduate.BookPharmacy.dto.client.ClientUpdateDto;
 import kr.KWGraduate.BookPharmacy.entity.Client;
 import kr.KWGraduate.BookPharmacy.exception.status.*;
 import kr.KWGraduate.BookPharmacy.repository.ClientRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.parameters.P;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -32,90 +39,45 @@ public class ClientService {
 
     private void validateDuplicateClient(Client client) {
 
-
-        if(isExistLoginId(client.getLoginId())){
+        if(clientRepository.existsByLoginId(client.getLoginId())){
             throw new ExistIdException("이미 id가 존재합니다.");
         }
-        if(isExistNickname(client.getNickname())){
+        if(clientRepository.existsByNickname(client.getNickname())){
             throw new ExistNicknameException("이미 존재하는 닉네임입니다.");
         }
-        if(isExistEmail(client.getEmail())){
+        if(clientRepository.existsByEmail(client.getEmail())){
             throw new ExistEmailException("이미 존재하는 이메일입니다.");
         }
     }
+    @Transactional
+    public void updateClient(ClientUpdateDto clientUpdateDto){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        AuthenticationAdapter principal = (AuthenticationAdapter)authentication.getPrincipal();
+        String username = principal.getUsername();
 
-    public boolean isExistLoginId(String loginId){
-        return clientRepository.existsByLoginId(loginId);
+        Client client = clientRepository.findByLoginId(username).get();
+        clientUpdateDto.setPassword((bCryptPasswordEncoder.encode(clientUpdateDto.getPassword())));
+
+        client.update(clientUpdateDto);
+
     }
-    public boolean isExistNickname(String nickname){ return clientRepository.existsByNickname(nickname); }
-    public boolean isExistEmail(String email){
-        return clientRepository.existsByEmail(email);
-    }
 
-    public ClientLoginDto Login(String loginId, String password){
-        //예외를 돌리는 것이 나은지
 
-        Client client = clientRepository.findByLoginId(loginId).orElseThrow(() -> new NoExistIdException("id가 존재하지 않습니다."));
-
-        if(client.isEqualPassword(password)){
-            return new ClientLoginDto(client.getLoginId(), client.getPassword());
-        }
-        else{
-            throw new IsNotSamePasswordException("password가 일치하지 않습니다.");
-        }
-    }
     @Transactional
     public void removeClient(Long id){
         clientRepository.deleteById(id);
     }
 
-    //여기서부터는 아직 쓰이지 않는 메소드
-    //book관련과 키워드 관련메소드도 생성해야함
-    //코드 수정해야함
-
     public Client findById(String id){
-        return clientRepository.findById(id).orElseThrow(() ->new NoSuchElementException());
+        return clientRepository.findById(id).orElseThrow(() -> new NoExistIdException("id가 존재하지 않음"));
     }
     public List<Client> findAll(){
         return clientRepository.findAll();
     }
 
-
-    @Transactional
-    public void updateClient(ClientDto clientDto){
-        Client findClient = clientRepository.findById(clientDto.getId()).orElseThrow(() -> new NoSuchElementException());
-
-        //optional
-        findClient.update(clientDto.getPassword(),clientDto.getNickname(),clientDto.getOccupation());
-    }
     public Long getClientsCount(){
         return clientRepository.count();
     }
-
-
-
-    public boolean setVerificationCode(String name, String email, String code){
-    // client에 코드 있어야함
-        //예외를 호출하는 것이 더 나을 듯?
-
-        if(!isExistEmail(email)){
-            return false;
-        }
-        Client client = clientRepository.findByEmail(email).get();
-
-        if(!client.isEqualName(name)){
-            return false;
-        }
-        //client에 코드 삽입
-        return true;
-    }
-    public String findByCode(String id, String code){
-        Client client = clientRepository.findById(id).get();
-        //client code비교
-        return client.getLoginId();
-    }
-
-
 
 
 }
