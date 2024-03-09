4d6b4cd61e61089362dc02980b0cea1a26f56cd4
simjaeyoun <spqjfgogo@gmail.com>
2024-03-10 01:54:16 +0900

    feat : 회원정보를 return하는 dto추가

--- src/main/java/kr/KWGraduate/BookPharmacy/dto/client/ClientResponseDto.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/dto/client/ClientResponseDto.java
@@ -4,8 +4,10 @@ import kr.KWGraduate.BookPharmacy.entity.Client;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
-public class ClientDto {
+public class ClientResponseDto {
 
     private String loginId;
     private String name;
@@ -13,40 +15,27 @@ public class ClientDto {
     private String email;
     private Client.Gender gender;
     private Client.Occupation occupation;
+    private LocalDate birth;
 
     @Builder
-    public ClientDto(Long id, String loginId, String password, String name, String nickname, String email, Client.Gender gender, Client.Occupation occupation) {
-        this.id = id;
+    public ClientResponseDto(String loginId,String name, String nickname, String email, Client.Gender gender, Client.Occupation occupation, LocalDate birth) {
         this.loginId = loginId;
-        this.password = password;
         this.name = name;
         this.nickname = nickname;
         this.email = email;
         this.gender = gender;
         this.occupation = occupation;
+        this.birth = birth;
     }
-    public Client toEntity(){
-        return Client.builder()
-                .id(id)
-                .loginId(loginId)
-                .password(password)
-                .name(name)
-                .nickname(nickname)
-                .email(email)
-                .gender(gender)
-                .occupation(occupation)
-                .build();
-    }
-    public ClientDto toDto(Client client){
-        return ClientDto.builder()
-                .id(client.getId())
+    public static ClientResponseDto toDto(Client client){
+        return ClientResponseDto.builder()
                 .loginId(client.getLoginId())
-                .password(client.getPassword())
                 .name(client.getName())
                 .nickname(client.getNickname())
                 .email(client.getEmail())
                 .gender(client.getGender())
                 .occupation(client.getOccupation())
+                .birth(client.getBirth())
                 .build();
     }
 }
