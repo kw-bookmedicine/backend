173ec997d42381a5b4c83b40095d11b53b0147b1
simjaeyoun <spqjfgogo@gmail.com>
2024-04-06 16:03:25 +0900

    Feat : 고민 게시판과 처방전entity 작성

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/entity/Prescription.java
@@ -0,0 +1,48 @@
+package kr.KWGraduate.BookPharmacy.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Prescription extends BaseTimeEntity{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prescription_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    String title;
+
+    String description;
+
+    @Builder
+    public Prescription(Client client, Book book, Board board, String title, String description){
+        this.client = client;
+        this.book = book;
+        this.board = board;
+        this.title = title;
+        this.description = description;
+    }
+
+    public void modifyPrescription(Book book, String title, String description){
+        this.book = book;
+        this.title = title;
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/kr/KWGraduate/BookPharmacy/repository/BoardRepository.java
@@ -0,0 +1,32 @@
+package kr.KWGraduate.BookPharmacy.repository;
+
+import kr.KWGraduate.BookPharmacy.entity.Board;
+import kr.KWGraduate.BookPharmacy.enums.Keyword;
+import kr.KWGraduate.BookPharmacy.enums.Status;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BoardRepository extends JpaRepository<Board,Long> {
+
+    @Query("select b from Board b join fetch b.client c where b.status = kr.KWGraduate.BookPharmacy.enums.Status.PRESCRIBING")
+    Slice<Board> findAllBoards(Pageable pageable);
+
+    @Query("select b from Board b join fetch b.client c where b.status = kr.KWGraduate.BookPharmacy.enums.Status.PRESCRIBING and b.keyword = :keyword")
+    Slice<Board> findByKeyword(Pageable pageable, @Param("keyword")Keyword keyword);
+
+    //검색에 키워드까지 추가
+    @Query("select b from Board b join fetch b.client c where (b.title like %:searchKeyword% or b.description like %:searchKeyword%) and b.status = kr.KWGraduate.BookPharmacy.enums.Status.PRESCRIBING")
+    Slice<Board> findByTitleContainingOrDescriptionContaining(Pageable pageable,@Param("searchKeyword") String searchKeyword);
+
+    @Query("select b from Board b join fetch b.client c where c.loginId = :loginId")
+    Slice<Board> findByUsername(Pageable pageable, @Param("loginId") String username);
+
+
+    @Query("select b from Board b join fetch b.client c where c.loginId = :loginId and b.status = :status")
+    Slice<Board> findByUsernameAndStatus(Pageable pageable, @Param("loginId") String username, @Param("status")Status status);
+
+    void deleteById(Long id);
+}
