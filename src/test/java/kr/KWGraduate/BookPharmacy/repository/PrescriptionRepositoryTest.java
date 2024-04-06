4ac8f996b8c342a885f3ab5ded873be0f41fe5a8
simjaeyoun <spqjfgogo@gmail.com>
2024-04-06 22:56:43 +0900

    Test : count 쿼리 테스트

--- src/test/java/kr/KWGraduate/BookPharmacy/repository/PrescriptionRepositoryTest.java
+++ src/test/java/kr/KWGraduate/BookPharmacy/repository/PrescriptionRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -47,7 +48,18 @@ class PrescriptionRepositoryTest {
                 .keyword(Keyword.Economy)
                 .build();
 
-        Board save = boardRepository.save(board1);
+
+
+        Board board2 = Board.builder()
+                .title("게시판2")
+                .client(client)
+                .description("asdfㅁㄴㅇㄻㄴㅇㄹ")
+                .keyword(Keyword.Economy)
+                .build();
+
+
+        boardRepository.save(board1);
+        boardRepository.save(board2);
 
         Prescription prescription1 = Prescription.builder()
                 .board(board1)
@@ -93,6 +105,53 @@ class PrescriptionRepositoryTest {
                 .title("나를 정말 사랑해?")
                 .build();
 
+
+        Prescription prescription7 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("여기")
+                .title("제목")
+                .build();
+        Prescription prescription8 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("너의 대한 나의 마음")
+                .title("얼마나 큰지")
+                .build();
+        Prescription prescription9 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("이하정")
+                .title("사랑해")
+                .build();
+
+        Prescription prescription10 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("우리")
+                .title("행복하자")
+                .build();
+        Prescription prescription11 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("내가 잘할게")
+                .title("근데 조금 마음이 아프네")
+                .build();
+        Prescription prescription12 = Prescription.builder()
+                .board(board2)
+                .book(book)
+                .client(client)
+                .description("이하정 너는")
+                .title("나를 정말 사랑해?")
+                .build();
+
+
+
         prescriptionRepository.save(prescription1);
         prescriptionRepository.save(prescription2);
         prescriptionRepository.save(prescription3);
@@ -100,8 +159,16 @@ class PrescriptionRepositoryTest {
         prescriptionRepository.save(prescription5);
         prescriptionRepository.save(prescription6);
 
+        prescriptionRepository.save(prescription7);
+        prescriptionRepository.save(prescription8);
+        prescriptionRepository.save(prescription9);
+        prescriptionRepository.save(prescription10);
+        prescriptionRepository.save(prescription11);
+        prescriptionRepository.save(prescription12);
 
-//        PageRequest pageRequest = PageRequest.of(0,12, Sort.by("createdDate"));
+
+
+        PageRequest pageRequest = PageRequest.of(0,12, Sort.by("createdDate"));
 //        for (Prescription prescription : prescriptionRepository.findByBoardId(pageRequest, 1L)) {
 //            System.out.println(prescription);
 //        }
@@ -111,8 +178,11 @@ class PrescriptionRepositoryTest {
 //        }
 //
 
-        Long l = prescriptionRepository.countByBoard(save.getId());
-        Assertions.assertThat(l).isEqualTo(6);
+        System.out.println("here");
+        List<Object[]> objects = prescriptionRepository.countByBoard();
+        for(var s : objects){
+            System.out.println(s[0] +" "+ s[1]);
+        }
     }
 
 }
