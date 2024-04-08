8458e3cea247293e943fe3c2b7c84fc99cc2aeaa
simjaeyoun <spqjfgogo@gmail.com>
2024-04-08 17:45:35 +0900

    Test : prescription쿼리를 수정함에 따른 테스트 코드 변경

--- src/test/java/kr/KWGraduate/BookPharmacy/repository/PrescriptionRepositoryTest.java
+++ src/test/java/kr/KWGraduate/BookPharmacy/repository/PrescriptionRepositoryTest.java
@@ -58,8 +58,10 @@ class PrescriptionRepositoryTest {
                 .build();
 
 
-        boardRepository.save(board1);
-        boardRepository.save(board2);
+        Board save = boardRepository.save(board1);
+        Long boardId1 = save.getId();
+        Board save1 = boardRepository.save(board2);
+        Long boardId2 = save1.getId();
 
         Prescription prescription1 = Prescription.builder()
                 .board(board1)
@@ -179,7 +181,7 @@ class PrescriptionRepositoryTest {
 //
 
         System.out.println("here");
-        List<Object[]> objects = prescriptionRepository.countByBoard();
+        List<Object[]> objects = prescriptionRepository.countByBoard(List.of(boardId1, boardId2));
         for(var s : objects){
             System.out.println(s[0] +" "+ s[1]);
         }
