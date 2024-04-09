c515c8ff843406649363e7a1f803b27a7f371001
simjaeyoun <spqjfgogo@gmail.com>
2024-04-09 14:08:38 +0900

    Feat : 처방전 service에 필요한 dto작성 1. 처방전 생성 2. 처방전 수정 3. 고민 게시판출력 4. 마이페이지 출력

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/dto/prescription/response/PrescriptionMyPageDto.java
@@ -0,0 +1,36 @@
+package kr.KWGraduate.BookPharmacy.dto.prescription.response;
+
+import kr.KWGraduate.BookPharmacy.entity.Prescription;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class PrescriptionMyPageDto {
+    private Long id;
+    private String title;
+    private String description;
+    private LocalDateTime createdDate;
+    private String nickname;
+    private String book_title;
+    private String author; // 저자명
+    private String publishingHouse; // 출판사명
+    private String publishYear; // 발행년도
+    private String imageUrl; // 이미지 url
+
+    @Builder
+    public PrescriptionMyPageDto(Prescription prescription){
+        this.id = prescription.getId();
+        this.title = prescription.getTitle();
+        this.description = prescription.getDescription();
+        this.createdDate = prescription.getCreatedDate();
+        this.nickname = prescription.getClient().getNickname();
+        this.book_title = prescription.getBook().getTitle();
+        this.author = prescription.getBook().getAuthor();
+        this.publishYear = prescription.getBook().getPublishYear();
+        this.imageUrl = prescription.getBook().getImageUrl();
+    }
+}
