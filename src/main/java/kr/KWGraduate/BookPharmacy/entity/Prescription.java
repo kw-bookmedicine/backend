8e2081f87262f68731379a61be71aa7caa8463ab
simjaeyoun <spqjfgogo@gmail.com>
2024-04-09 14:09:49 +0900

    Test : 처방전 service테스트 , 테스트를 위한 처방전 entity tostring추가

--- src/main/java/kr/KWGraduate/BookPharmacy/entity/Prescription.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/entity/Prescription.java
@@ -1,14 +1,12 @@
 package kr.KWGraduate.BookPharmacy.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@ToString(of = {"id" , "title" , "description"})
 public class Prescription extends BaseTimeEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "prescription_id")
