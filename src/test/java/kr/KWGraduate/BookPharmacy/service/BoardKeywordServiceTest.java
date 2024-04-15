f16258c27a5312fee1a30929d586c8c268eb5574
simjaeyoun <spqjfgogo@gmail.com>
2024-04-15 17:19:53 +0900

    [Test] 1. 모든 키워드를 조회하는 테스트 추가 2. 해당 키워드의 질문과 객관식을 조회하는 테스트 추가

---
+++ src/test/java/kr/KWGraduate/BookPharmacy/service/BoardKeywordServiceTest.java
@@ -0,0 +1,55 @@
+package kr.KWGraduate.BookPharmacy.service;
+
+import kr.KWGraduate.BookPharmacy.dto.boardkeyword.BoardKeywordDto;
+import kr.KWGraduate.BookPharmacy.dto.boardkeyword.BoardQuestionAndDistractorDto;
+import kr.KWGraduate.BookPharmacy.entity.Keyword;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BoardKeywordServiceTest {
+    @Autowired
+    BoardKeywordService boardKeywordService;
+
+    @Test
+    void 모든_키워드_추출_메서드(){
+        List<BoardKeywordDto> expectedKeywords = Arrays.asList(
+                new BoardKeywordDto(Keyword.Economy_Management, "경제/경영"),
+                new BoardKeywordDto(Keyword.Health, "건강"),
+                new BoardKeywordDto(Keyword.Children_Parenting, "자녀/양육"),
+                new BoardKeywordDto(Keyword.Employment_Career, "취업"),
+                new BoardKeywordDto(Keyword.Workbook_Examination, "문제집/수험서"),
+                new BoardKeywordDto(Keyword.Relationships_Communication, "관계/소통"),
+                new BoardKeywordDto(Keyword.Fiction_Essays, "소셜/에세이"),
+                new BoardKeywordDto(Keyword.Philosophy, "철학"),
+                new BoardKeywordDto(Keyword.History, "역사"),
+                new BoardKeywordDto(Keyword.Science_Math_Engineering, "수학/과학/공학"),
+                new BoardKeywordDto(Keyword.Society, "사회"),
+                new BoardKeywordDto(Keyword.Hobbies, "취미")
+        );
+        List<BoardKeywordDto> boardKeywords = boardKeywordService.getBoardKeywords();
+
+        org.junit.jupiter.api.Assertions.assertIterableEquals(boardKeywords,expectedKeywords);
+
+    }
+
+    @Test
+    void 특정_키워드_질문_객관식_추출(){
+        List<BoardQuestionAndDistractorDto> expectedList = Keyword.Economy_Management.getQuesAndDis().stream()
+                .map(BoardQuestionAndDistractorDto::new)
+                .collect(Collectors.toList());
+
+        List<BoardQuestionAndDistractorDto> questionAndDistractor = boardKeywordService.getQuestionAndDistractor(Keyword.Economy_Management);
+
+        Assertions.assertIterableEquals(expectedList,questionAndDistractor);
+        System.out.println(expectedList);
+    }
+}
