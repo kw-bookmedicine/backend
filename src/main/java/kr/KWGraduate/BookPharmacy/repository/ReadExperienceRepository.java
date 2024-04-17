b7e58cc26b34eea0643798b475bd47db9f56b1bb
seonghooni <lsh3717@naver.com>
2024-04-17 23:50:24 +0900

    [Refactor] Repository 계층 독서경험 페이징 조회 작성

--- src/main/java/kr/KWGraduate/BookPharmacy/repository/ReadExperienceRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/repository/ReadExperienceRepository.java
@@ -1,6 +1,8 @@
 package kr.KWGraduate.BookPharmacy.repository;
 
 import kr.KWGraduate.BookPharmacy.entity.ReadExperience;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,12 +16,19 @@ public interface ReadExperienceRepository extends JpaRepository<ReadExperience, 
 
     /**
      * 유저 정보로 읽은 경험을 모두 조회하기
-     * (나의 독서 경험 관리하기)에서 사용
      * (머신러닝 과정)에서 사용
      */
     @EntityGraph(attributePaths = {"book"})
     @Query("select re from ReadExperience re join fetch re.client c where c.loginId = :loginId")
     List<ReadExperience> findByLoginId(@Param("loginId") String clientLoginId);
+
+    /**
+     * 유저 정보로 읽은 경험을 모두 조회하기 (페이징)
+     * (나의 독서 경험 관리하기)에서 사용
+     */
+    @EntityGraph(attributePaths = {"book"})
+    @Query("select re from ReadExperience re join fetch re.client c where c.loginId = :loginId")
+    Page<ReadExperience> findPagingByLoginId(@Param("loginId") String clientLoginId, Pageable pageable);
 
     /**
      * 유저 정보와 책 정보로 조회하기
