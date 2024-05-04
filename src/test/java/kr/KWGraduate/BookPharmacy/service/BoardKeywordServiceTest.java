9d059fcdd9b1c191b21ef99f7b529835fc34087c
simjaeyoun <spqjfgogo@gmail.com>
2024-05-04 17:37:22 +0900

    [Test] service코드 수정으로 인한 테스트 코드 변경

--- src/test/java/kr/KWGraduate/BookPharmacy/service/BoardKeywordServiceTest.java
+++ src/test/java/kr/KWGraduate/BookPharmacy/service/BoardKeywordServiceTest.java
@@ -1,6 +1,5 @@
 package kr.KWGraduate.BookPharmacy.service;
 
-import kr.KWGraduate.BookPharmacy.dto.boardkeyword.BoardKeywordDto;
 import kr.KWGraduate.BookPharmacy.dto.boardkeyword.BoardQuestionAndDistractorDto;
 import kr.KWGraduate.BookPharmacy.entity.Keyword;
 import org.junit.jupiter.api.Assertions;
@@ -8,11 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BoardKeywordServiceTest {
@@ -21,21 +17,23 @@ class BoardKeywordServiceTest {
 
     @Test
     void 모든_키워드_추출_메서드(){
-        List<BoardKeywordDto> expectedKeywords = Arrays.asList(
-                new BoardKeywordDto(Keyword.Economy_Management, "경제/경영"),
-                new BoardKeywordDto(Keyword.Health, "건강"),
-                new BoardKeywordDto(Keyword.Children_Parenting, "자녀/양육"),
-                new BoardKeywordDto(Keyword.Employment_Career, "취업"),
-                new BoardKeywordDto(Keyword.Workbook_Examination, "문제집/수험서"),
-                new BoardKeywordDto(Keyword.Relationships_Communication, "관계/소통"),
-                new BoardKeywordDto(Keyword.Fiction_Essays, "소셜/에세이"),
-                new BoardKeywordDto(Keyword.Philosophy, "철학"),
-                new BoardKeywordDto(Keyword.History, "역사"),
-                new BoardKeywordDto(Keyword.Science_Math_Engineering, "수학/과학/공학"),
-                new BoardKeywordDto(Keyword.Society, "사회"),
-                new BoardKeywordDto(Keyword.Hobbies, "취미")
-        );
-        List<BoardKeywordDto> boardKeywords = boardKeywordService.getBoardKeywords();
+//        List<BoardKeywordDto> expectedKeywords = Arrays.asList(
+//                new BoardKeywordDto(Keyword.Economy_Management, "경제/경영"),
+//                new BoardKeywordDto(Keyword.Health, "건강"),
+//                new BoardKeywordDto(Keyword.Children_Parenting, "자녀/양육"),
+//                new BoardKeywordDto(Keyword.Employment_Career, "취업"),
+//                new BoardKeywordDto(Keyword.Workbook_Examination, "문제집/수험서"),
+//                new BoardKeywordDto(Keyword.Relationships_Communication, "관계/소통"),
+//                new BoardKeywordDto(Keyword.Fiction_Essays, "소셜/에세이"),
+//                new BoardKeywordDto(Keyword.Philosophy, "철학"),
+//                new BoardKeywordDto(Keyword.History, "역사"),
+//                new BoardKeywordDto(Keyword.Science_Math_Engineering, "수학/과학/공학"),
+//                new BoardKeywordDto(Keyword.Society, "사회"),
+//                new BoardKeywordDto(Keyword.Hobbies, "취미")
+//        );
+
+        List<String> expectedKeywords = List.of("경제/경영","건강","자녀/양육","취업","문제집/수험서","관계/소통","소셜/에세이","철학","역사","수학/과학/공학","사회","취미");
+        List<String> boardKeywords = boardKeywordService.getBoardKeywords();
 
         org.junit.jupiter.api.Assertions.assertIterableEquals(boardKeywords,expectedKeywords);
 
@@ -47,7 +45,7 @@ class BoardKeywordServiceTest {
                 .map(BoardQuestionAndDistractorDto::new)
                 .collect(Collectors.toList());
 
-        List<BoardQuestionAndDistractorDto> questionAndDistractor = boardKeywordService.getQuestionAndDistractor(Keyword.Economy_Management);
+        List<BoardQuestionAndDistractorDto> questionAndDistractor = boardKeywordService.getQuestionAndDistractor("경제/경영");
 
         Assertions.assertIterableEquals(expectedList,questionAndDistractor);
         System.out.println(expectedList);
