17d48a0f142c6a1a62c3f8345cabc7837294cdd1
seonghooni <lsh3717@naver.com>
2024-05-09 22:01:05 +0900

    [Refactor] global exception 양식에 맞게 Client 예외처리 수정

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/client/exception/NoExistIdException.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/client/exception/NoExistIdException.java
@@ -1,9 +1,10 @@
 package kr.KWGraduate.BookPharmacy.domain.client.exception;
 
-import kr.KWGraduate.BookPharmacy.global.common.error.AllException;
+import kr.KWGraduate.BookPharmacy.global.common.error.ResourceNotFoundException;
 
-public class NoExistIdException extends AllException {
-    public NoExistIdException(String errorMessage){
-        super(errorMessage);
+public class NoExistIdException extends ResourceNotFoundException {
+
+    public NoExistIdException(String target){
+        super("there is not such id : " + target);
     }
 }
