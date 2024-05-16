adc1367850ab3aa2bef3da619887761721d2d185
simjaeyoun <spqjfgogo@gmail.com>
2024-05-16 21:50:19 +0900

    [Feat] 패스워드 수정하는 dto생성

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/client/dto/request/ClientPasswordUpdateDto.java
@@ -0,0 +1,10 @@
+package kr.KWGraduate.BookPharmacy.domain.client.dto.request;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ClientPasswordUpdateDto {
+    private String password;
+}
