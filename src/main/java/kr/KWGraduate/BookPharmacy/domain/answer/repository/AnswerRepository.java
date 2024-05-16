476591e88d3da61dcc698eb460f96cdf87f54e32
simjaeyoun <spqjfgogo@gmail.com>
2024-05-16 23:40:17 +0900

    [Feat] boardId입력에 따른 삭제 추가

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/answer/repository/AnswerRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/answer/repository/AnswerRepository.java
@@ -3,6 +3,7 @@ package kr.KWGraduate.BookPharmacy.domain.answer.repository;
 import kr.KWGraduate.BookPharmacy.domain.answer.domain.Answer;
 import kr.KWGraduate.BookPharmacy.domain.keyword.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -14,4 +15,8 @@ public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
     @Query("select a from Answer a join fetch a.board b where b.keyword = :keyword")
     List<Answer> findByKeyword(@Param("keyword") Keyword keyword);
+
+    @Modifying
+    @Query("delete from Answer a where a.board.id = :boardId")
+    void deleteByBoardId(@Param("boardId")Long boardId);
 }
--- src/main/java/kr/KWGraduate/BookPharmacy/domain/prescription/repository/PrescriptionRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/prescription/repository/PrescriptionRepository.java
@@ -4,6 +4,7 @@ import kr.KWGraduate.BookPharmacy.domain.prescription.domain.Prescription;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -20,6 +21,8 @@ public interface PrescriptionRepository extends JpaRepository<Prescription, Long
     @Query("select b.id, count(p) from Prescription p join p.board b where b.id in :boardIds group by b.id")
     List<Object[]> countByBoard(@Param("boardIds") List<Long> boardIds);
 
-
+    @Modifying
+    @Query("delete from Prescription p where p.board.id = :boardId")
+    void deleteByBoardId(@Param("boardId") Long boardId);
     void deleteById(Long id);
 }
