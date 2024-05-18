56910d8380085d12a2d4d9c333c088319a681ed3
이성훈 <lsh3717@naver.com>
2024-05-18 21:49:38 +0900

    [Merge] 2024-05-18 버전 추가 적용 (#148)
    * [Feat] 한줄처방 검색어 또는 키워드로 조회 기능 추가 (#147)
    
    ---------
    
    Signed-off-by: 이성훈 <lsh3717@naver.com>
    Signed-off-by: simjaeyoun <104803819+simjaeyoun@users.noreply.github.com>
    Co-authored-by: simjaeyoun <104803819+simjaeyoun@users.noreply.github.com>

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/onelineprescription/api/OneLinePrescriptionController.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/onelineprescription/api/OneLinePrescriptionController.java
@@ -2,6 +2,9 @@ package kr.KWGraduate.BookPharmacy.domain.onelineprescription.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.KWGraduate.BookPharmacy.domain.board.dto.response.BoardConcernPageDto;
+import kr.KWGraduate.BookPharmacy.domain.keyword.domain.Keyword;
+import kr.KWGraduate.BookPharmacy.domain.keyworditem.dto.response.KeywordItemDto;
 import kr.KWGraduate.BookPharmacy.domain.onelineprescription.dto.request.OneLineCreateDto;
 import kr.KWGraduate.BookPharmacy.domain.onelineprescription.dto.request.OneLineUpdateDto;
 import kr.KWGraduate.BookPharmacy.domain.onelineprescription.dto.response.OneLineResponseDto;
@@ -15,6 +18,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/oneline-prescriptions")
@@ -41,6 +46,30 @@ public class OneLinePrescriptionController {
                                                                               @AuthenticationPrincipal UserDetails userDetails) {
         PageRequest pageRequest = PageRequest.of(page, size, Sort.by("createdDate").descending());
         Page<OneLineResponseDto> result = oneLinePrescriptionService.getMyOneLinePrescriptions((AuthenticationAdapter) userDetails, pageRequest);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/keyword")
+    @Operation(summary = "한줄처방 키워드 별 조회", description = "무한 스크롤을 위한 size와 page입력 필수, 키워드 입력")
+    public ResponseEntity<Page<OneLineResponseDto>> getOneLinePrescriptionsByKeyword(@RequestParam(name = "keyword") Keyword keyword,
+                                                                                     @RequestParam("page") int page,
+                                                                                     @RequestParam("size") int size)
+    {
+        PageRequest pageRequest = PageRequest.of(page,size, Sort.by("createdDate").descending());
+        Page<OneLineResponseDto> result = oneLinePrescriptionService.getOneLinePrescriptionsByKeyword(keyword, pageRequest);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @Operation(summary = "검색어를 포함하는 한줄처방 리스트를 페이징 조회", description = "무한 스크롤을 위한 size와 page입력 필수, 검색어 입력")
+    @GetMapping(value = "/search")
+    public ResponseEntity<Page<OneLineResponseDto>> getOneLinePrescriptionsBySearch(@RequestParam(name = "name") String searchWord,
+                                                                                @RequestParam("page") int page,
+                                                                                @RequestParam("size") int size)
+    {
+        PageRequest pageRequest = PageRequest.of(page,size, Sort.by("createdDate").descending());
+        Page<OneLineResponseDto> result = oneLinePrescriptionService.getOneLinePrescriptionsBySearch(searchWord, pageRequest);
 
         return ResponseEntity.ok(result);
     }
