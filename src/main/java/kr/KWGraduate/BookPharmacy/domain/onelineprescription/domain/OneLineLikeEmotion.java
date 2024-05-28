29a65dcd81217d95939b5e8e44354715a4f4b94b
seonghooni <lsh3717@naver.com>
2024-05-29 00:50:46 +0900

    [Feat] 한줄처방 좋아요/도움이되었어요 엔티티 추가

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/onelineprescription/domain/OneLineLikeEmotion.java
@@ -0,0 +1,35 @@
+package kr.KWGraduate.BookPharmacy.domain.onelineprescription.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import kr.KWGraduate.BookPharmacy.domain.client.domain.Client;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OneLineLikeEmotion {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "one_line_like_emotion_id")
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "one_line_prescription_id")
+    private OneLinePrescription oneLinePrescription;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id")
+    private Client client;
+
+    @Builder
+    public OneLineLikeEmotion(OneLinePrescription oneLinePrescription, Client client) {
+        this.oneLinePrescription = oneLinePrescription;
+        this.client = client;
+    }
+}
