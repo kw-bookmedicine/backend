72d8b49cc128fd63eace32fa07c977fe4dd253ed
simjaeyoun <spqjfgogo@gmail.com>
2024-05-31 22:23:20 +0900

    [Refactor] 1. 클라이언트 관계를 manytoOne으로 변경 2. 빌더 생성

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/book/domain/ClientRecommend.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/book/domain/ClientRecommend.java
@@ -3,6 +3,7 @@ package kr.KWGraduate.BookPharmacy.domain.book.domain;
 import jakarta.persistence.*;
 import kr.KWGraduate.BookPharmacy.domain.client.domain.Client;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.OnDelete;
@@ -16,7 +17,7 @@ public class ClientRecommend {
     @Column(name = "client_recommend_id")
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "client_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     private Client client;
@@ -26,5 +27,12 @@ public class ClientRecommend {
     private Book book;
 
     private Integer rank;
+
+    @Builder
+    public ClientRecommend(Client client, Book book, Integer rank){
+        this.client = client;
+        this.book = book;
+        this.rank = rank;
+    }
 
 }
