9ee3a037595bec809bf0c9d99c03ac1f5ee17eec
simjaeyoun <spqjfgogo@gmail.com>
2024-06-01 19:49:02 +0900

    [Feat] redis에 저장될 사용자 정보

---
+++ src/main/java/kr/KWGraduate/BookPharmacy/global/infra/redis/oauth2/Oauth2SignUpInfo.java
@@ -0,0 +1,29 @@
+package kr.KWGraduate.BookPharmacy.global.infra.redis.oauth2;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import java.util.UUID;
+
+@Getter
+@RedisHash(value = "refreshToken",timeToLive = 3600)
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Oauth2SignUpInfo {
+    @Id
+    private String email;
+    private String name;
+    private String providerId;
+    private String password;
+
+    @Builder
+    public Oauth2SignUpInfo(String email, String name, String providerId){
+        this.email = email;
+        this.name = name;
+        this.providerId = providerId;
+        this.password = Integer.toString((email + name + providerId).hashCode());
+    }
+}
