2aa253c12ccf66e4d2262b313f7ee9271f01d662
이성훈 <lsh3717@naver.com>
2024-06-03 15:47:51 +0900

    [Merge] 2024-06-03 버전 추가 적용 (#188)

--- src/test/java/kr/KWGraduate/BookPharmacy/service/ClientServiceTest.java
+++ src/test/java/kr/KWGraduate/BookPharmacy/service/ClientServiceTest.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
