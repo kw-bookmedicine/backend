a57d9b485c23da863885bde1283544b856cf4549
seonghooni <lsh3717@naver.com>
2024-06-04 17:11:31 +0900

    [Refactor] 조인이 아닌 조회수 컬럼을 참조하도록 코드 변경

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/onelineprescription/repository/OneLineLikeEmotionRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/onelineprescription/repository/OneLineLikeEmotionRepository.java
@@ -9,10 +9,6 @@ import java.util.Optional;
 
 public interface OneLineLikeEmotionRepository extends JpaRepository<OneLineLikeEmotion, Long> {
 
-    // 유저가 '좋아요'를 눌렀는지 확인하기 위함
-    @Query("select ol from OneLineLikeEmotion ol join fetch ol.client c where c.loginId = :loginId")
-    OneLineLikeEmotion findByLoginId(@Param("loginId") String loginId);
-
     @Query("select ol from OneLineLikeEmotion ol join fetch ol.client c join fetch ol.oneLinePrescription op " +
             "where c.loginId = :loginId " +
             "and op.id = :oneLineId")
