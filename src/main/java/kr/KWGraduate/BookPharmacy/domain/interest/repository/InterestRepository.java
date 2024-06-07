39ef1a0ee7749315b6907c2d713dde7ce4caa72f
이성훈 <lsh3717@naver.com>
2024-06-07 18:09:22 +0900

    [Merge] 2024-06-07 버전 적용 (#218)
    * [Feat] 관심사 기반 추천 스케쥴링 코드 작성 (#217)
    
    * [Feat] 관심사 기반 추천 스케쥴링 코드 작성
    
    * [Refactor] 독서경험 BaseTimeEntity 상속하지 않도록 수정
    
    * [Refactor] 관심사 id 가져오는 코드 오류 수정
    
    * [Refactor] 서브모듈 업데이트 06/07
    
    ---------
    
    Signed-off-by: 이성훈 <lsh3717@naver.com>
    Signed-off-by: simjaeyoun <104803819+simjaeyoun@users.noreply.github.com>
    Co-authored-by: simjaeyoun <104803819+simjaeyoun@users.noreply.github.com>

--- src/main/java/kr/KWGraduate/BookPharmacy/domain/interest/repository/InterestRepository.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/domain/interest/repository/InterestRepository.java
@@ -4,6 +4,7 @@ import kr.KWGraduate.BookPharmacy.domain.interest.domain.Interest;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -13,5 +14,5 @@ public interface InterestRepository extends JpaRepository<Interest, Long> {
     // 유저의 아이디로 관심사를 조회함
     @EntityGraph(attributePaths = {"category"})
     @Query("select i from Interest i join fetch i.client c where c.loginId = :loginId")
-    List<Interest> findByLoginId(String loginId);
+    List<Interest> findByLoginId(@Param("loginId") String loginId);
 }
--- src/main/java/kr/KWGraduate/BookPharmacy/global/common/service/BookSchedulerService.java
+++ src/main/java/kr/KWGraduate/BookPharmacy/global/common/service/BookSchedulerService.java
@@ -1,6 +1,7 @@
 package kr.KWGraduate.BookPharmacy.global.common.service;
 
 import kr.KWGraduate.BookPharmacy.domain.book.service.BestSellerBookService;
+import kr.KWGraduate.BookPharmacy.domain.book.service.RecommendService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,15 @@ import java.time.LocalDateTime;
 public class BookSchedulerService {
 
     private final BestSellerBookService bestSellerBookService;
+    private final RecommendService recommendService;
 
     @Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 매일 서울기준, 00시 00분 00초에 실행
     public void setBestSeller() {
         bestSellerBookService.setBestSellerBooks();
+    }
+
+    @Scheduled(cron = "1 0 0 * * *", zone = "Asia/Seoul") // 매일 서울기준, 00시 00분 00초에 실행
+    public void setInterestRecommend() {
+        recommendService.setInterestRecommend();
     }
 }
